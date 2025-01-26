edee14a7fd85a4223d11297539b45d45dfe83c73
CollinPoetoehena <collinpoetoehena@gmail.com>
2025-01-26 17:26:23 +0100

    refactor: set to 10 seconds interval for compression

--- energy-efficiency/experiments/execute_experiments.py
+++ energy-efficiency/experiments/execute_experiments.py
@@ -97,7 +97,7 @@ def run_experiment(archetype: str, output_dir, exp_rep):
             print("Waiting before next action...")
             # Set to higher interval for compression specifically between actions due to failures otherwise 
             # (other implementations lower intervals works fine, but compression this was required, likely due to higher execution times)
-            time.sleep(9)
+            time.sleep(10)
 
     # Before measuring the active energy, make sure the active period has passed for equal comparisons
     elapsed_time = time.time() - active_start_time
